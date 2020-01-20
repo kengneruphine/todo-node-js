52ef8664335590f81a44820d136ccf129565eca2
kengneruphine <ruphinekengne@gmail.com>
2020-01-20 13:41:28 +0100

    creating a todo app with nodeJs

---
+++ seeders/20200120121432-demo-todo.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Todos', [{
+      note: 'Hello Todo',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }, {
+      note: 'Another Todo',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Todos', null, {});
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
